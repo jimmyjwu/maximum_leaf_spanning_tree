04239573de1a3bdcab8b00a9dace1c777cc5d3bc
Jimmy Wu <jimmywu126@gmail.com>
2013-04-30 00:54:07 -0700

    Created framework for building hard graph instances.

--- python/graph_generator.py
+++ python/graph_generator.py
@@ -1,3 +1,43 @@
 from graph import *
 
-graph = Graph(200)
+# Create graph with 100 nodes
+
+
+
+# Populate the graph with up to 2000 edges
+# Choose edges such that there exists a leafy tree within, but hard to find
+
+
+# Returns a hard graph instance
+def create_hard_graph():
+	leafy_tree = create_leafy_tree(100)
+	hard_graph = graph_containing_tree(leafy_tree)
+
+	return hard_graph
+
+
+# Returns a tree with many leaves
+def create_leafy_tree(number_of_nodes):
+	tree = Graph(100)
+
+	# Populate the tree with appropriate edges
+
+
+
+	return tree
+
+
+
+# Takes a tree and returns a general graph that contains the tree and obscures its leaves.
+def graph_containing_tree(tree):
+	# Add edges to tree so as to make its leaves hard to find
+
+
+
+	return tree
+
+
+
+
+# Outputs the graph to a text file in the format given by instructors
+def output_graph_to_text_file(graph):
