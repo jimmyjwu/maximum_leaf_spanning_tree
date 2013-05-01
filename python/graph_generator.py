6b7883863fb75bd412544738efdfcb5d52d895f7
Jimmy Wu <jimmywu126@gmail.com>
2013-04-30 18:28:46 -0700

    Implemented generation of random graphs of user-specified size.

--- python/graph_generator.py
+++ python/graph_generator.py
@@ -44,6 +44,9 @@ def create_leafy_tree(number_of_nodes):
 
 
 
+		return
+
+
 	# Populate the tree with appropriate edges
 	build_leafy_tree(0, number_of_nodes)
 
@@ -56,7 +59,7 @@ def create_leafy_tree(number_of_nodes):
 def graph_containing_tree(tree):
 
 	# Maintain a random-ordered list of unused edges
-	unused_edges = find_unused_edges(tree)
+	unused_edges = get_unused_edges(tree)
 	shuffle(unused_edges)
 
 	# Add random edges to tree to obscure its leaves
@@ -64,14 +67,83 @@ def graph_containing_tree(tree):
 
 	return tree
 
+# Returns a list of edges in the graph
+def get_edges(graph):
+	number_of_nodes = len(graph.neighbors)
+	edge_set = set()
+
+	for current_node in range(0, number_of_nodes):
+		for adjacent_node in graph.neighbors[current_node]:
+			edge_set.add(Edge(current_node, adjacent_node))
+
+	return list(edge_set)
+
+
 # Returns a list of edges not used in the graph.
-def find_unused_edges(graph):
-	unused_edges = []
+def get_unused_edges(graph):
+	number_of_nodes = len(graph.neighbors)
+	unused_edge_set = set()
+
+	for current_node in range(0, number_of_nodes):
+		current_node_neighbors = set(graph.neighbors[current_node])
+		for other_node in range(0, number_of_nodes):
+			if current_node != other_node:
+				if other_node not in current_node_neighbors:
+					unused_edge_set.add(Edge(current_node, other_node))
+
+	return list(unused_edge_set)
 
 
-	return unused_edges
+# Returns a random graph of given size
+def create_sample_graph(number_of_nodes, number_of_edges):
+
+	# For an undirected graph with n nodes, m = n(n-1)/2
+	if number_of_edges > number_of_nodes * (number_of_nodes - 1) / 2:
+		number_of_edges = number_of_nodes * (number_of_nodes - 1) / 2
+
+	# Create empty graph
+	sample_graph = Graph(number_of_nodes)
+
+	# Maintain a random-order list of unused edges
+	unused_edges = get_unused_edges(sample_graph)
+	shuffle(unused_edges)
+
+	# Add random edges to graph
+	for _ in range(number_of_edges):
+		sample_graph.add_edge(unused_edges.pop())
+
+	return sample_graph
+
+
+# Outputs several graphs to a text file in the format given by instructors
+def output_graphs_to_new_file(graphs, file_name):
+	output_file = open(file_name, 'w')
+
+	number_of_graphs = len(graphs)
+	output_file.write(str(number_of_graphs) + '\n')
+
+	for graph in graphs:
+		output_graph_to_existing_file(graph, output_file)
+
+	output_file.close()
 
 
 
 # Outputs the graph to a text file in the format given by instructors
-def output_graph_to_text_file(graph):
+def output_graph_to_existing_file(graph, output_file):
+
+	# Build a list of distinct edges
+	edges = get_edges(graph)
+
+	# Output number of edges in this graph to file
+	number_of_edges = len(edges)
+	output_file.write(str(number_of_edges) + '\n')
+
+	# Output all the edges in this graph to file
+	for edge in edges:
+		u = edge.ends[0]
+		v = edge.ends[1]
+		output_file.write(str(u) + ' ' + str(v) + '\n')
+
+
+
