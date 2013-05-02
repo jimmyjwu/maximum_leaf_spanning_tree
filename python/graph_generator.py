b34f12a054b6588334e7099e5198754f36a67538
Jimmy Wu <jimmywu126@gmail.com>
2013-05-01 21:21:02 -0700

    Made progress on generating hard graphs.

--- python/graph_generator.py
+++ python/graph_generator.py
@@ -1,5 +1,5 @@
 from graph import *
-from random import shuffle
+from random import shuffle, randint
 
 """
 Our objective is to build a "hard instance" graph such that our output spanning
@@ -16,35 +16,67 @@ generation of this graph, then the tree we built will be the MLST for the graph
 (or at least an exceptionally leafy tree).
 """
 
+# Define constants
+MAXIMUM_DEGREE = 5
+CONSTANT_CONSTRUCTION = 1234
+RANDOM_CONSTRUCTION = 2345
+
 
 # Returns a hard graph instance
-def create_hard_graph(number_of_nodes):
-	leafy_tree = create_leafy_tree(number_of_nodes)
+def create_hard_graph(number_of_nodes, construction_type):
+
+	# Define the degree function for constant tree construction
+	def get_constant_branch_and_leaf_factors():
+		branch_factor = 4
+		leaf_factor = 5
+		return [branch_factor, leaf_factor]
+
+	# Define the degree function for randomized tree construction
+	def get_randomized_branch_and_leaf_factors():
+		degree = randint(2, MAXIMUM_DEGREE)
+		random_factor = random(1, degree)
+		leaf_factor = max(random_factor, degree - random_factor)
+		branch_factor = degree - leaf_factor
+		return [branch_factor, leaf_factor]
+
+	# Select the correct degree function
+	degree_function = None
+	if construction_type == CONSTANT_CONSTRUCTION:
+		degree_function = get_constant_branch_and_leaf_factors
+	elif construction_type == RANDOM_CONSTRUCTION:
+		degree_function = get_randomized_branch_and_leaf_factors
+
+	# Build a leafy tree
+	leafy_tree = create_leafy_tree(number_of_nodes, degree_function)
+
+	# Build a graph out of the tree
 	hard_graph = graph_containing_tree(leafy_tree)
 
 	return hard_graph
 
 
 # Returns a tree with many leaves
-def create_leafy_tree(number_of_nodes):
+def create_leafy_tree(number_of_nodes, get_branch_and_leaf_factors):
 
 	# Create an empty graph
 	tree = Graph(number_of_nodes)
 
-	# Maintain a random-ordered list of unused leaves
-	unused_leaves = [i for i in range(1, number_of_nodes)]
-	shuffle(unused_leaves)
+	# Maintain a random-ordered list of unused nodes
+	unused_nodes = [i for i in range(1, number_of_nodes)]
+	shuffle(unused_nodes)
+
+	# Maintain a queue of nodes not yet expanded
+	nodes_to_expand = []
+
+	# Start the tree at some node
+	nodes_to_expand.append(unused_nodes.pop())
+
+	# Expand the tree until it reaches all nodes
+	while len(nodes_to_expand) > 0:
+		current_node = nodes_to_expand.pop(0)
 
 
-	# Define a helper function to recursively build the tree
-	def build_leafy_tree(root_node, number_of_nodes):
-		# Create some leaves
 
-		# Branch out by recursively calling build_leafy_tree()
-
-
-
-		return
 
 
 	# Populate the tree with appropriate edges
@@ -66,6 +98,7 @@ def graph_containing_tree(tree):
 
 
 	return tree
+
 
 # Returns a list of edges in the graph
 def get_edges(graph):
@@ -92,6 +125,15 @@ def get_unused_edges(graph):
 					unused_edge_set.add(Edge(current_node, other_node))
 
 	return list(unused_edge_set)
+
+# Returns a list of all nodes in the tree that are leaves (degree one)
+def get_leaves(tree):
+	leaves = []
+	for node in range(0, len(tree.neighbors)):
+		if len(tree.neighbors[node]) == 1:
+			leaves.append(node)
+
+	return leaves
 
 
 # Returns a random graph of given size
