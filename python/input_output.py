af106be0a7da473af64ac6b2fb910bc56891a2af
Jimmy Wu <jimmywu126@gmail.com>
2013-05-04 03:41:31 -0700

    Added function to read in graphs from file.

--- python/input_output.py
+++ python/input_output.py
@@ -1,8 +1,46 @@
 from graph import *
 from graph_helper import *
 from constants import *
+from collections import deque
+
+# Reads and returns all graphs in the given text file
+# NOTE: Graphs must be in format given by instructors
+def input_graphs_from_file(file_name):
+
+	# Read in raw lines
+	with open(file_name) as input_file:
+		raw_lines = input_file.readlines()
+
+	# Strip newline from each line
+	for i in range(len(raw_lines)):
+		raw_lines[i] = raw_lines[i].strip()
+
+	# Store lines in a queue (using deque) for efficient processing
+	lines = deque(raw_lines)
+
+	# Maintain a list of graphs
+	graphs = []
+
+	# Read lines in nested structure
+	number_of_graphs = int(lines.popleft())
+	for _ in range(number_of_graphs):
+		edges = []
+		number_of_edges = int(lines.popleft())
+
+		for _ in range(number_of_edges):
+			edge_ends = lines.popleft().split()
+			u = int(edge_ends[0])
+			v = int(edge_ends[1])
+			edges.append(Edge(u, v))
+
+		graph = make_graph(edges)
+		graphs.append(graph)
+
+	return graphs
+
 
 # Outputs several graphs to a text file in the format given by instructors
+# NOTE: Overwrites existing file with same name
 def output_graphs_to_new_file(graphs, file_name):
 	output_file = open(file_name, 'w')
 
@@ -30,3 +68,4 @@ def output_graph_to_existing_file(graph, output_file):
 		u = edge.ends[0]
 		v = edge.ends[1]
 		output_file.write(str(u) + ' ' + str(v) + '\n')
+
